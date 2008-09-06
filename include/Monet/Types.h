a8dac8de25ba2709d706eff7de4f19a2ffc9e8d1
Wim Vander Schelden <wim@fixnum.org>
2008-09-06 11:03:47 +0200

    Forgot to add Types.h

---
+++ include/Monet/Types.h
@@ -0,0 +1,13 @@
+#import <stdint.h>
+
+#ifndef UInt8
+typedef uint8_t	UInt8;
+typedef uint16_t UInt16;
+typedef uint32_t UInt32;
+typedef uint64_t UInt64;
+
+typedef int8_t  SInt8;
+typedef int16_t SInt16;
+typedef int32_t SInt32;
+typedef int64_t SInt64;
+#endif
