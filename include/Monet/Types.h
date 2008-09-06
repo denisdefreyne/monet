3d289dd01db14798cd036b54df1e37f354ae442e
Denis Defreyne <denis.defreyne@stoneship.org>
2008-09-06 11:04:58 +0200

    made Types.h only be included on Linux

--- include/Monet/Types.h
+++ include/Monet/Types.h
@@ -1,6 +1,7 @@
+#if !defined(__APPLE__)
+
 #import <stdint.h>
 
-#ifndef UInt8
 typedef uint8_t	UInt8;
 typedef uint16_t UInt16;
 typedef uint32_t UInt32;
@@ -10,4 +11,5 @@ typedef int8_t  SInt8;
 typedef int16_t SInt16;
 typedef int32_t SInt32;
 typedef int64_t SInt64;
+
 #endif
