3b0b96740401fecffcd5662e869616e5046b7cb6
Denis Defreyne <denis.defreyne@stoneship.org>
2011-12-31 16:34:32 +0100

    added MOPoint{Add,Subtract}

--- include/Monet/MOPoint.h
+++ include/Monet/MOPoint.h
@@ -10,6 +10,16 @@ static inline MOPoint MOPointMake(float aX, float aY)
 	return vec2_create(aX, aY);
 }
 
+static inline MOPoint MOPointAdd(MOPoint aPoint1, MOPoint aPoint2)
+{
+	return vec2_add(aPoint1, aPoint2);
+}
+
+static inline MOPoint MOPointSubtract(MOPoint aPoint1, MOPoint aPoint2)
+{
+	return vec2_sub(aPoint1, aPoint2);
+}
+
 static inline float MOPointDistanceTo(MOPoint aPoint1, MOPoint aPoint2)
 {
 	return vec2_dist(aPoint1, aPoint2);
